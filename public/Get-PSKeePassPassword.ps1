7a7b5b2eda0559140ce3dd0270f03f4d4b185c93
Dan Lee <del001@uark.edu>
2018-09-16 23:38:31 -0500

    Get-PSKeePassPassword DynamicParameter

--- public/Get-PSKeePassPassword.ps1
+++ public/Get-PSKeePassPassword.ps1
@@ -3,10 +3,40 @@ Function Get-PSKeePassPassword {
     param(
         $DatabasePath = $Script:Config.KeePassDatabasePath,
         $DatabasePassword = (Import-Clixml $Script:Config.KeePassDatabasePasswordFile).GetNetworkCredential().Password,
-        $Title,
         [Switch]$Show
     )
+    DynamicParam {
+            # Set the dynamic parameters' name
+            $ParameterName = 'Title'
+            
+            # Create the dictionary 
+            $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+            # Create the collection of attributes
+            $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            
+            # Create and set the parameters' attributes
+            $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $ParameterAttribute.Mandatory = $true
+            $ParameterAttribute.Position = 1
+
+            # Add the attributes to the attributes collection
+            $AttributeCollection.Add($ParameterAttribute)
+
+            # Generate and set the ValidateSet 
+            $arrSet = Get-PSKeePassTitles
+            $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+
+            # Add the ValidateSet to the attributes collection
+            $AttributeCollection.Add($ValidateSetAttribute)
+
+            # Create and return the dynamic parameter
+            $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+            $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+            return $RuntimeParameterDictionary
+    }
     Begin {
+        $Title = $PSBoundParameters[$ParameterName]
         $Database = New-PSKeePassConnection -DatabasePath $DatabasePath -DatabasePassword $DatabasePassword
     }
     Process{
